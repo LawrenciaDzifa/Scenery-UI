07607d7969ac39f07b803dfe4ccc475599da182d
LawrenciaDzifa <dzifa01ahiadeke@gmail.com>
2022-04-05 15:00:49 +0000

    updated bottom nav bar

--- lib/Screens/activities.dart
+++ lib/Screens/activities.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:scenery/Screens/activity.dart';
 import 'package:scenery/Screens/grid.dart';
+import 'package:scenery/Screens/profile.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,14 +9,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int currentIndex = 0;
+  final screens = [Home(), Activities(), Profile()];
 
   @override
   Widget build(BuildContext context) {
@@ -29,106 +24,93 @@ class _HomeState extends State<Home> {
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
-            backgroundColor: Colors.black26,
-            appBar: AppBar(
-                elevation: 5.0,
-                centerTitle: true,
-                backgroundColor: Colors.black87,
-                title: Center(
-                    child: Text(
-                  'Explore',
-                  style: TextStyle(color: Colors.white24, fontSize: 21.0),
-                )),
-                leading: IconButton(
-                    icon: Icon(Icons.menu, color: Colors.white24),
-                    onPressed: () {}),
-                actions: [
-                  Row(
-                    children: [
-                      IconButton(
-                          icon: Icon(
-                            Icons.notifications_none_outlined,
-                            color: Colors.white24,
-                          ),
-                          onPressed: () {}),
-                      IconButton(
-                          icon: Icon(
-                            Icons.search_outlined,
-                            color: Colors.white24,
-                          ),
-                          onPressed: () {})
-                    ],
-                  ),
-                ],
-                bottom: TabBar(
-                  indicatorColor: Colors.pink,
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicatorPadding: EdgeInsets.only(left: 20.0, right: 20.0),
-                  indicatorWeight: 3.5,
-                  tabs: [
-                    Tab(
-                      child: Text(
-                        'DISCOVER',
-                        style: TextStyle(
-                            color: Colors.white24,
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        'ACTIVITIES',
-                        style: TextStyle(
-                            color: Colors.white24,
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
+          backgroundColor: Colors.black26,
+          appBar: AppBar(
+              elevation: 5.0,
+              centerTitle: true,
+              backgroundColor: Colors.black87,
+              title: Center(
+                  child: Text(
+                'Explore',
+                style: TextStyle(color: Colors.white60, fontSize: 21.0),
+              )),
+              leading: IconButton(
+                  icon: Icon(Icons.menu, color: Colors.white60),
+                  onPressed: () {}),
+              actions: [
+                Row(
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.notifications_none_outlined,
+                          color: Colors.white60,
+                        ),
+                        onPressed: () {}),
+                    IconButton(
+                        icon: Icon(
+                          Icons.search_outlined,
+                          color: Colors.white60,
+                        ),
+                        onPressed: () {})
                   ],
-                  labelColor: Colors.pink,
-                )),
-            bottomNavigationBar: BottomNavyBar(
-              backgroundColor: Colors.black,
-              selectedIndex: _currentIndex,
-              showElevation: true,
-              itemCornerRadius: 18.0,
-              curve: Curves.easeIn,
-              onItemSelected: (index) => setState(() => _currentIndex = index),
-              items: <BottomNavyBarItem>[
-                BottomNavyBarItem(
-                  icon: Icon(Icons.home_outlined, color: Colors.white24),
-                  title: Text('Home'),
-                  activeColor: Colors.pink.shade300,
-                  inactiveColor: Colors.transparent,
-                  textAlign: TextAlign.center,
-                ),
-                BottomNavyBarItem(
-                  icon: Icon(Icons.place_outlined, color: Colors.white24),
-                  title: Text('Places'),
-                  activeColor: Colors.pink.shade300,
-                  inactiveColor: Colors.transparent,
-                  textAlign: TextAlign.center,
-                ),
-                BottomNavyBarItem(
-                  icon: Icon(Icons.message_outlined,color: Colors.white24),
-                  title: Text(
-                    'Forum',
-                  ),
-                  activeColor: Colors.pink.shade300,
-                  inactiveColor: Colors.transparent,
-                  textAlign: TextAlign.center,
-                ),
-                BottomNavyBarItem(
-                  icon: Icon(Icons.person_outlined, color: Colors.white24),
-                  title: Text('Account'),
-                  activeColor: Colors.pink.shade300,
-                  inactiveColor: Colors.green,
-                  textAlign: TextAlign.center,
                 ),
               ],
-            ),
-            body: Grid(),
-            ),
+              bottom: TabBar(
+                indicatorColor: Colors.pink,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorPadding: EdgeInsets.only(left: 20.0, right: 20.0),
+                indicatorWeight: 3.5,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      'DISCOVER',
+                      style: TextStyle(
+                          color: Colors.white60,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'ACTIVITIES',
+                      style: TextStyle(
+                          color: Colors.white60,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+                labelColor: Colors.pink,
+              )),
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              iconSize: 30,
+              backgroundColor: Colors.black87,
+              unselectedFontSize: 14,
+              selectedItemColor: Colors.pink,
+              unselectedItemColor: Colors.grey,
+              currentIndex: currentIndex,
+              onTap: (index) => setState(() => currentIndex = index),
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  label: 'HOME',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: 'SETTINGS',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'PROFILE',
+                ),
+                // BottomNavigationBarItem(
+                //   icon: Icon(Icons.person_outlined),
+                //   label: 'Profile',
+                // )
+              ]),
+          body: Grid(),
+        ),
       ),
     );
   }
---
+++ lib/Screens/activity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Activities extends StatefulWidget {
+  const Activities({Key key}) : super(key: key);
+
+  @override
+  _ActivitiesState createState() => _ActivitiesState();
+}
+
+class _ActivitiesState extends State<Activities> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
